cda7d7be9adba2725f0d9a8c48ff6e0e971ad261
Silas de Araujo <silasdearaujo@live.com>
2021-03-17 21:10:29 -0300

    styles botoes(radiobuttons)/alimentando table

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,13 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
+import { api } from '../../services/api';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+
+import { Container, TransactionTypeContainer, Button } from './styles';
 
 interface INewTransactionModalProps {
   isOpen: boolean;
@@ -9,6 +15,25 @@ interface INewTransactionModalProps {
 }
 
 export function NewTransationModal({ isOpen, onRequestClose }: INewTransactionModalProps) {
+  const [type, setType] = useState('income');
+  
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+      event.preventDefault();
+
+      const data = {
+        title,
+        value,
+        category,
+        type
+      }
+
+      api.post('/transactions', data);
+  }
+
   return (
       <Modal 
         isOpen={isOpen}
@@ -23,12 +48,48 @@ export function NewTransationModal({ isOpen, onRequestClose }: INewTransactionMo
             src={closeImg} 
             alt="Fechar"/>
         </button>
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
           <h2>Cadastrar transação</h2>
 
-          <input placeholder="Título"/>
-          <input type="number" placeholder="Valor"/>
-          <input placeholder="Categoria"/>
+          <input 
+            placeholder="Título" 
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+          <input 
+            type="number" 
+            placeholder="Valor" 
+            value={value}
+            onChange={event => setValue(Number(event.target.value))}
+          />
+
+          <TransactionTypeContainer>
+            <Button
+              type="button"  
+              onClick={() => { setType('income'); }}
+              isActive={type === 'income'}
+              activeColor="green"
+            >
+              <img src={incomeImg} alt="Entrada"/>
+              <span>Entrada</span>
+            </Button>
+
+            <Button
+              type="button"  
+              onClick={() => { setType('outcome'); }}
+              isActive={type === 'outcome'}
+              activeColor="red"
+            >
+              <img src={outcomeImg} alt="Saída"/>
+              <span>Saída</span>
+            </Button>
+          </TransactionTypeContainer>
+
+          <input 
+            placeholder="Categoria" 
+            value={category}
+            onChange={event => setCategory(event.target.value)}
+          />
 
           <button type="submit">Cadastrar</button>
         </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento website',
+          type: 'income',
+          category: 'Dev',
+          amount: 5000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Desenvolvimento website',
+          type: 'income',
+          category: 'Dev',
+          amount: 2000,
+          createdAt: new Date('2021-03-12 09:00:00')
+        },
+        {
+          id: 3,
+          title: 'Aluguel',
+          type: 'outcome',
+          category: 'Casa',
+          amount: 1000,
+          createdAt: new Date('2021-02-05 09:00:00')
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 540,
-          type: 'inctome',
-          categoty: 'hora extra',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
